aedd779c7992a4b990fec8822ab88418cd685828
parasaurolophus <apps@rader.us>
2023-09-06 13:59:46 -0500

    Improve contracts and documentation

--- README.md
+++ README.md
@@ -1,15 +1,116 @@
 # timer/timer
 
-Interval timers from [Racket](https://racket-lang.org/) threads
+Interval timers from [Racket] threads
 
-| Procedure                   | Description                                                                                    |
-|-----------------------------|------------------------------------------------------------------------------------------------|
-| `start-timer`               | Invoke a thunk in a separate thread after at least a given number of milliseconds have elapsed |
-| `call-with-timer-semaphore` | Invoke a thunk in the current thread with the semaphore associated with a given timer locked    |
-| `kill-timer`                | Kill a timer thread                                                                            |
-| `timer?`                    | Return `#t` if and only if a given object is a timer                                           |
+A [Racket] module that provides:
 
-## Usage
+- `call-with-timer-semaphore`
+- `join-timer`
+- `kill-timer`
+- `start-timer`
+- `timer?`
+- `timer-thread`
+- `timer-semaphore`
+
+Timer objects are created using `start-timer`:
+
+```scheme
+(define my-timer (start-timer 3000.0
+                              (lambda (message)
+                                  (display message) (newline))
+                              #f
+                              "expired"))
+```
+
+The preceding will write `expired` followed by a newline to the
+standard output port in a separate thread after at least three seconds
+have elapsed. The returned timer object can be used to further
+interact with the timer's thread. Note that for compatibility with
+Racket's built in `call-with-semaphore`, `start-timer` takes an
+optional `try-fail-handler` thunk and a _rest_ argument that can be
+used to parameterize the procedure invoked by the worker thread. In
+this case, however, the `try-fail-handler` will never be invoked
+because the semaphore will be created and immediately locked by the
+calling thread before even creating the worker thread. This might
+change in some future version of this library module if the need
+arises to support passing in an existing semaphore.
+
+Use `timer?` to determine whether or not any given value is a timer:
+
+```scheme
+(timer? my-timer) ; => #t
+(timer? 42) ; => #f
+```
+
+Each timer object is an association between a semaphore and a worker
+thread. The worker thread will invoke the procedure passed to
+`start-timer` with the semaphore locked after at least the specified
+number of milliseconds have elapsed. The thread is created with the
+semaphore initially locked by the calling thread until `start-timer`
+returns. It is then re-locked by the worker thread when the timer
+expires for the duration of the call to the timer-handler procedure.
+
+The `call-with-timer-semaphore` procedure can be used by other threads
+to execute code with the same semaphore locked as that used by a
+timer's worker thread.
+
+```scheme
+(define my-result (call-with-timer-semaphore
+                      my-timer
+                      (lambda (message)
+                          (display message)
+                          (newline)
+                          42)
+                      #f
+                      "in main thread"))
+my-result ; => 42
+```
+
+Like `start-timer`, `call-with-timer-semaphore` implements a contract
+identical to that of Racket's built-in `call-with-semaphore` other
+than the data type of the first argument. Unlike `start-timer`, the
+`try-fail-handler` argument to `call-with-timer-semaphore` is
+effective to guard against possible race conditions between the
+calling thread and the given timer's worker thread.
+
+Use `join-timer` to block the calling thread until a given timer's
+worker thread terminates:
+
+```scheme
+(join-timer my-timer)
+```
+
+Use `kill-timer` to forcibly terminate a timer whether or not it has
+yet expired:
+
+```scheme
+(kill-timer my-timer)
+```
+
+For completeness, the following timer struct accessors are exported:
+
+``` scheme
+(timer-semaphore my-timer) ; => semaphore
+(timer-thread my-timer) ; => thread
+```
+
+Beware of using the struct accessors in ways that might break the
+timer encapsulation.
+
+## Pre-Requisites
+
+Working _Racket_ installation:
+<https://docs.racket-lang.org/getting-started/index.html>
+
+## Building
+
+```bash
+git clone git@github.com:parasaurolophus/racket-timer.git
+cd racket-timer
+raco pkg install --name timer --link $PWD
+```
+
+## Examples
 
 ### Racket
 
@@ -22,10 +123,17 @@ Interval timers from [Racket](https://racket-lang.org/) threads
 (define timer (start-timer 3000.0 (lambda () (display "expired\n"))))
 
 ; show that timer? returns true for an object returned by start-timer
-(timer? timer) => #t
+(display (timer? timer))
+(newline)
 
 ; write "in main thread" to stdout with the timer semaphore locked
-(call-with-timer-semaphore timer (lambda () (display "in main thread\n")))
+(call-with-timer-semaphore
+    timer
+    (lambda (message)
+        (display message)
+        (newline))
+    #f
+    "in main thread")
 
 ; kill the timer thread
 (kill-timer timer)
@@ -33,7 +141,11 @@ Interval timers from [Racket](https://racket-lang.org/) threads
 
 ### R<sup>6</sup>RS
 
-Replace the first three lines in the above with:
+The main reason this module exists is to provide a convenience wrapper
+around native Racket features, packaged in a way that makes them
+accessible from other languages supported by the Racket runtime. For
+example, to define the preceding in R<sup>6</sup>RS Scheme, simply
+replace the first three lines in the above with:
 
 ```scheme
 #!r6rs
@@ -41,14 +153,9 @@ Replace the first three lines in the above with:
 (import (rnrs) (timer timer))
 ```
 
-## Pre-Requisites
+The rest of the example is already R<sup>6</sup>RS compatible. You can
+use `start-timer`, `call-with-timer-semaphore` etc. even though you
+will not be able to access the underlying thread and semaphore
+primitives directly.
 
-Working _Racket_ installation: <https://docs.racket-lang.org/getting-started/index.html>
-
-## Building
-
-```bash
-git clone git@github.com:parasaurolophus/racket-timer.git
-cd racket-timer
-raco pkg install --name timer --link $PWD
-```
+[Racket]: https://racket-lang.org/
--- timer.rkt
+++ timer.rkt
@@ -2,36 +2,142 @@
 
 ;; Copyright Kirk Rader 2023
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; timer/timer
+;;
+;; Interval timers from Racket threads
+;;
+;; Provides:
+;;
+;; - call-with-timer-semaphore
+;; - join-timer
+;; - kill-timer
+;; - start-timer
+;; - timer?
+;; - timer-semaphore
+;; - timer-thread
+;;
+;; Timer objects are created using `start-timer`
+;;
+;;     (start-timer milliseconds procedure try-fail-handler arguments ...)
+;;
+;; Use `timer?` to determine whether or not any given value is a timer.
+;;
+;;     (timer? v)
+;;
+;; Each timer object is an association between a semaphore and a
+;; worker thread. The worker thread will invoke the procedure passed
+;; to `start-timer` with the semaphore locked after at least the
+;; specified number of milliseconds have elapsed. The thread is
+;; created with the semaphore initially locked by the calling thread
+;; until `start-timer` returns. It is then re-locked by the worker
+;; thread when the timer expires for the duration of the call to the
+;; timer-handler procedure.
+;;
+;; The `call-with-timer-semaphore` procedure can be used by other
+;; threads to execute code with the same semaphores locked as those
+;; which are used by timer worker threads.
+;;
+;;     (call-with-timer-semaphore
+;;         timer
+;;         procedure
+;;         try-fail-handler
+;;         arguments ...)
+;;
+;; Both `start-timer` and `call-with-timer-semaphore` implement
+;; contracts identical to that of Racket's built-in
+;; `call-with-semaphore`, which they both invoke internally, other
+;; than the data types of their first arguments. Note that the
+;; `try-fail-handler` argument to `start-timer` is actually not
+;; necessary since it will never be invoked in the current
+;; implementation. It is provided for consistency and to future-proof
+;; the contract in case it ever becomes desirable to support passing
+;; in an existing semaphore when creating a timer. The
+;; `try-fail-handler` argument to `call-with-timer-semaphore` is
+;; effective to guard against possible race conditions between the
+;; calling thread and the given timer's worker thread.
+;;
+;; Use `join-timer` to block the calling thread until a given timer's
+;; worker thread terminates.
+;;
+;;     (join-timer timer)
+;;
+;; Use `kill-timer` to forcibly terminate a timer whether or not it
+;; has yet expired.
+;;
+;;     (kill-timer timer)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Require scribble inline documentation support modules
 (require scribble/srcdoc
          (for-doc scribble/base scribble/manual))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; call-with-timer-semaphore
+
+(provide
+ (proc-doc
+  call-with-timer-semaphore
+  (->i ([timer timer?]
+        [procedure procedure?])
+       ([try-fail-thunk (or/c procedure? #f)])
+       #:rest [arguments (listof any/c)]
+       [result any/c])
+  (#f)
+  ("Apply " (racket procedure) " to the given " (racket arguments) 
+            ", with the semaphore associated with " (racket timer)
+            " locked")))
+(define (call-with-timer-semaphore timer
+                                   procedure
+                                   (try-fail-thunk #f)
+                                   .
+                                   arguments)
+  (apply call-with-semaphore (list* (timer-semaphore timer)
+                                    procedure
+                                    try-fail-thunk
+                                    arguments)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; join-timer
+
 (provide
  (proc-doc/names
-  call-with-timer-semaphore
-  (timer? procedure? . -> . any)
-  (timer thunk)
-  ("Invoke " (racket thunk) " with the semaphore associated with "
-             (racket timer) " locked")))
-(define (call-with-timer-semaphore timer thunk)
-  (call-with-semaphore (timer-semaphore timer) thunk))
+  join-timer
+  (-> timer? void?)
+  (timer)
+  ("Block the calling thread until the " (racket timer) " thread terminates")))
+(define (join-timer timer)
+  (thread-wait (timer-thread timer)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; kill-timer
 
 (provide
  (proc-doc/names
   kill-timer
-  (timer? . -> . void?)
+  (-> timer? void?)
   (timer)
   ("Kill the " (racket thread) " running " (racket timer))))
 (define (kill-timer timer)
   (kill-thread (timer-thread timer)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; start-timer
+
 (provide
- (proc-doc/names
+ (proc-doc
   start-timer
-  ((and/c real? positive?) procedure? . -> . timer?)
-  (milliseconds thunk)
-  ("Invoke " (racket thunk) " in a separate thread after at least "
-             (racket milliseconds) " have elasped.")))
-(define (start-timer milliseconds thunk)
+  (->i ([milliseconds (and/c real? positive?)]
+        [procedure procedure?])
+       ([try-fail-thunk (or/c procedure? #f)])
+       #:rest [arguments (listof any/c)]
+       [result any/c])
+  (#f)
+  ("Start a worker thread with an associated semaphore "
+   "that will apply " (racket procedure) " to the given "
+   (racket arguments) " after at least " (racket milliseconds)
+   "have elapsed")))
+(define (start-timer milliseconds procedure (try-fail-thunk #f) . arguments)
   (let ((s (make-semaphore 1)))
     (call-with-semaphore
      s
@@ -39,16 +145,30 @@
        (let ((t (thread
                  (lambda ()
                    (sleep (/ milliseconds 1000.0))
-                   (call-with-semaphore s thunk)))))
+                   (apply call-with-semaphore
+                          (list* s procedure try-fail-thunk arguments))))))
          (timer t s))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; timer?
 
 (provide
  (proc-doc/names
   timer?
-  (any/c . -> . boolean?)
+  (-> any/c boolean?)
   (v)
   ("Return " (racket #t) " if and only if " (racket v) " is a timer object")))
-(struct timer
-  ;; Associate a thread and semaphore.
-  (thread
-   semaphore))
+(provide
+ (proc-doc/names
+  timer-thread
+  (-> timer? thread?)
+  (timer)
+  ("Return the " (racket thread) " associated with a given " (racket timer))))
+(provide
+ (proc-doc/names
+  timer-semaphore
+  (-> timer? semaphore?)
+  (timer)
+  ("Return the " (racket semaphore) " associated with a given "
+                 (racket timer))))
+(struct timer (thread semaphore))
